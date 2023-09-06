2088f7d086f958b5416229a07e9e680471974dc2
pat-s <patrick.schratz@gmail.com>
2023-09-06 23:39:03 +0200

    dont comment on releases

--- release-config.ts
+++ release-config.ts
@@ -1,11 +1,14 @@
 export default {
+  commentOnReleasedPullRequests: false,
   beforePrepare: async ({ exec, nextVersion }) => {
     await exec(`apk add --no-cache helm curl`);
-    await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" otf-agent/Chart.yaml`);
-    await exec(`export UPSTREAM_VERSION=$(curl -s https://api.github.com/repos/leg100/otf/releases/latest | grep tag_name | grep -Eo "([0-9]+(\.[0-9]+)+)") && sed -i "s/^appVersion:.*$/appVersion: $UPSTREAM_VERSION/g" otf-agent/Chart.yaml`);
-    await exec("helm dependency update");
     await exec(
-      "git add otf-agent/Chart.yaml"
+      `sed -i "s/^version:.*$/version: ${nextVersion}/g" otf-agent/Chart.yaml`
     );
+    await exec(
+      `export UPSTREAM_VERSION=$(curl -s https://api.github.com/repos/leg100/otf/releases/latest | grep tag_name | grep -Eo "([0-9]+(\.[0-9]+)+)") && sed -i "s/^appVersion:.*$/appVersion: $UPSTREAM_VERSION/g" otf-agent/Chart.yaml`
+    );
+    await exec("helm dependency update");
+    await exec("git add otf-agent/Chart.yaml");
   },
 };
