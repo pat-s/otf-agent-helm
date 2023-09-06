4aca0a6516b5c7f6d5320c6e5d045f9d6ed5a570
pat-s <patrick.schratz@gmail.com>
2023-09-06 15:06:31 +0200

    release helper

--- release-config.ts
+++ release-config.ts
@@ -1,10 +1,10 @@
 export default {
   beforePrepare: async ({ exec, nextVersion }) => {
     await exec(`apk add --no-cache helm`);
-    await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" Chart.yaml`);
+    await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" otf-agent/Chart.yaml`);
     await exec("helm dependency update");
     await exec(
-      "git add Chart.yaml"
+      "git add otf-agent/Chart.yaml"
     );
   },
 };
