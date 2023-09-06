0cfc0f32bc3167bba188f0f51d13e47ca2faf78c
pat-s <patrick.schratz@gmail.com>
2023-09-06 15:06:31 +0200

    release helper

--- release-config.ts
+++ release-config.ts
@@ -1,10 +1,11 @@
 export default {
   beforePrepare: async ({ exec, nextVersion }) => {
-    await exec(`apk add --no-cache helm`);
-    await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" Chart.yaml`);
+    await exec(`apk add --no-cache helm curl`);
+    await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" otf-agent/Chart.yaml`);
+    // await exec(`export UPSTREAM_VERSION=$(curl -s https://api.github.com/repos/leg100/otf/releases/latest | grep tag_name | grep -Eo "([0-9]+(\.[0-9]+)+)") &7 sed -i "s/^appVersion:.*$/appVersion: ${UPSTREAM_VERSION}/g" otf-agent/Chart.yaml`);
     await exec("helm dependency update");
     await exec(
-      "git add Chart.yaml"
+      "git add otf-agent/Chart.yaml"
     );
   },
 };
