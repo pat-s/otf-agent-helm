65306fa008e8abd36b103cd8e0005ad2c59df5ef
pat-s <patrick.schratz@gmail.com>
2023-10-24 18:31:19 +0200

    fix release config and bump version

--- release-config.ts
+++ release-config.ts
@@ -8,9 +8,10 @@ export default {
       `sed -i "s/^version:.*$/version: ${nextVersion}/g" otf-agent/Chart.yaml`
     );
     await exec(
-      `export UPSTREAM_VERSION=$(curl -s https://api.github.com/repos/leg100/otf/releases/latest | grep tag_name | grep -Eo "([0-9]+(\.[0-9]+)+)") && sed -i "s/^appVersion:.*$/appVersion: $UPSTREAM_VERSION/g" otf-agent/Chart.yaml`
+      `export UPSTREAM_VERSION=$(curl -s https://api.github.com/repos/leg100/otf/releases/latest | grep tag_name | grep -Eo "([0-9]+(\.[0-9]+)+)") && sed -i "s/^appVersion:.*$/appVersion: $UPSTREAM_VERSION/g" charts/otf-agent/Chart.yaml`
     );
-    await exec("helm dependency update");
-    await exec("git add otf-agent/Chart.yaml");
+    await exec("helm dependency update charts/otf-agent");
+    await exec("curl -L -O https://github.com/norwoodj/helm-docs/releases/download/v1.11.3/helm-docs_1.11.3_Linux_x86_64.tar.gz && tar -xzf helm-docs_1.11.3_Linux_x86_64.tar.gz && chmod +x helm-docs && ./helm-docs");
+    await exec("git add charts/otf-agent/Chart.yaml && git add charts/otf-agent/README.md");
   },
 };
